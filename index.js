9f09dc1c53f470ed628d2c1f6098c36125ce38b2
Aisha-Yusuf <ayshaofficiel@gmail.com>
2023-10-14 17:07:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Make a fetch request to the Game of Thrones API
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data))
+    .catch((error) => console.log("Error fetching data:", error));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+function renderBooks(data) {
+  const bookList = document.getElementById("book-list");
+  data.forEach((book) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks(); // Call the fetchBooks function when the page is loaded
+
+
+
+
+
